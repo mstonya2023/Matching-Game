1b6a6edf304e01ecb0c9e00c5f5abf32d2859363
mstonya2023 <latonya.davis08@gmail.com>
2023-08-23 10:13:31 -0700

    was able to render front and back images on page

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 /*----- constants -----*/
-//  Audio countdown timer for clock
+//  Audio countdown timer for clock``
 // Each of the card objects will used twice
 const SOURCE_CARDS = [
-	{img: 'https://imgur.com/Fw2Xmek.jpg'	,matched: false},
-	{img: 'https://imgur.com/8somayG.jpg'	,matched: false},
-	{img: 'https://imgur.com/GME3odj.jpg'	,matched: false},
-	{img: 'https://imgur.com/7By2XLx.jpg'	, matched: false},
-	{img: 'https://imgur.com/rvGcGes.jpg'	, matched: false},
-	{img: 'https://imgur.com/0S1Suxa.jpg'	, matched: false}
+	{ img: 'imgs/tan.png', matched: false },
+	{ img: 'imgs/blue.png', matched: false },
+	{ img: 'imgs/green.png', matched: false },
+	{ img: 'imgs/orange.png', matched: false },
+	{ img: 'imgs/rainbow.png', matched: false },
+	{ img: 'imgs/red.png', matched: false },
+	
 ];
 
-const CARD_BACK = 'https://imgur.com/nhNQQMq.jpg' 
+const CARD_BACK = 'imgs/black.png';
 
 
 /*----- state variables -----*/
 let cards; //Array of 12 shuffled card objects
-let firstcardChoice; 
+let firstcardChoice;
 
 /*----- cached elements  -----*/
 
@@ -27,31 +28,33 @@ let firstcardChoice;
 
 // Initialize all state and then call render//
 
-init ();
+init();
 
 function init() {
 	cards = getShuffledCards();
 	firstcardChoice = null;
 	render();
 }
-	function render()	{
-		cards.forEach(function(card,idx) {
+function render() {
+	cards.forEach(function (card, idx) {
 		const imgEl = document.getElementById(idx);
-			imgEl.src = card.img
-		});	
-	}
+		const src = (cards.matched || card === firstcardChoice)  ? card.img : CARD_BACK ;
+		imgEl.src = src;
+	});
+}
 
 function getShuffledCards() {
 	let tmpCards = [];
-		let cards= [];
-	for (let card of SOURCE_CARDS) { 
+	let cards = [];
+	for (let card of SOURCE_CARDS) {
 		tmpCards.push(card, card);
 	}
 	while (tmpCards.length) {
 		let rndIdx = Math.floor(Math.random() * tmpCards.length);
-		let card = tmpCards.splice(rndIdx, 1)[0];
-	}	cards.push(card);
-
+		let plycard = tmpCards.splice(rndIdx, 1)[0];
+		cards.push(plycard);
+	}
+	return cards;
 
 }
 
