830a0012d8e66e5ac251a3db6b96772ec04bfe62
mstonya2023 <latonya.davis08@gmail.com>
2023-08-24 12:21:33 -0700

    testing updates to design

--- js/main.js
+++ js/main.js
@@ -8,22 +8,24 @@ const SOURCE_CARDS = [
 	{ img: 'imgs/orange.png', matched: false },
 	{ img: 'imgs/rainbow.png', matched: false },
 	{ img: 'imgs/red.png', matched: false },
-	
+
 ];
 
-const CARD_BACK = 'imgs/black.png';
+const CARD_BACK = 'imgs/gold.png';
 
 
 /*----- state variables -----*/
 let cards; //Array of 12 shuffled card objects
 let firstcardChoice;
 let ignoreClks;
+let secondcardChoice;
+let gameStatus;
 /*----- cached elements  -----*/
-const messageEl= document.querySelector('h3');
-
+const messageEl = document.querySelector('h3');
+const buttonEl = document.querySelector('button');
 /*----- event listeners -----*/
 document.querySelector('main').addEventListener('click', handChoice);
-
+buttonEl.addEventListener('click', init );
 /*----- functions -----*/
 
 // Initialize all state and then call render//
@@ -35,22 +37,35 @@ function init() {
 	firstcardChoice = null;
 	badNum = 0; //initialize
 	ignoreClks = false;
+	gameStatus = null;
+	buttonEl.style.visibility = 'hidden';
 	render();
 }
 function render() {
 	cards.forEach(function (card, idx) {
 		const imgEl = document.getElementById(idx);
-		const src = (cards.matched || card === firstcardChoice)  ? card.img : CARD_BACK ;
+		const src = (card.matched || card === firstcardChoice || card === secondcardChoice) ? card.img : CARD_BACK;
 		imgEl.src = src;
-		messageEl.innerHTML = `Bad Count: ${badNum}`;
 	});
+	if (gameStatus === 'win') {
+		messageEl.innerHTML = `YOU WIN!`;
+		buttonEl.style.visibility = 'visible';
+		
+	} else if (gameStatus === 'lose'){
+		messageEl.innerHTML = `YOU LOSE! TRY AGAIN! Bad Count: ${badNum}`;
+		buttonEl.style.visibility = 'visible';
+		
+	} else {
+		buttonEl.style.visibility = 'hidden';
+		messageEl.innerHTML = `Bad Count: ${badNum}`;
+	}
 }
 
 function getShuffledCards() {
 	let tmpCards = [];
 	let cards = [];
 	for (let card of SOURCE_CARDS) {
-		tmpCards.push({...card}, {...card});
+		tmpCards.push({ ...card }, { ...card });
 	}
 	while (tmpCards.length) {
 		let rndIdx = Math.floor(Math.random() * tmpCards.length);
@@ -63,23 +78,41 @@ function getShuffledCards() {
 //Update state and then call render//
 function handChoice(evt) {
 	const cardIdx = parseInt(evt.target.id);
-	if (isNaN(cardIdx) || ignoreClks ) return;
+	if (isNaN(cardIdx) || ignoreClks || gameStatus) return;
 	const card = cards[cardIdx];
 	if (firstcardChoice) {
+		secondcardChoice = card;
+		console.log(secondcardChoice);
 		if (firstcardChoice.img === card.img) {
-			firstcardChoice.matched = card.matched = true;
+			firstcardChoice.matched = secondcardChoice.matched = true;
 			firstcardChoice = null;
+			secondcardChoice = null;
+			if(cards.every(card => card.matched === true)){
+				gameStatus = 'win';
+			}
 		} else {
 			badNum++;
+			if (badNum >= 10) {
+				gameStatus = 'lose';
+			}
+			const timeOut = setTimeout(function flipTiles() {
+				console.log('hi');
+				firstcardChoice = null;
+				secondcardChoice = null;
+				render ();
+			}, 600);
+
 		}
-	
-	}else {
-	firstcardChoice = card;
+
+	} else {
+		firstcardChoice = card;
 
 	}
 	render();
 }
 
+
+	
 
 
 
